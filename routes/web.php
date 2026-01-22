ba065212318fe503bfe1c673b35aedf34a4675ce
PenJR <pinatacan.j@gmail.com>
2026-01-23 00:51:26 +0800

    Initial commit: My first Laravel portfolio

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+$name = 'John Rio D. Pinatacan'; 
+$course = 'Bachelor of Science in Information Technology';
+$university = 'College of Information Sciences and Computing'; 
+return view('about', [
+'name' => $name,
+'course' => $course,
+'university' => $university
+]);
+})->name('about');
+
+Route::get('/projects', function () {
+$projects = [
+['title' => 'Project 1', 'description' => 'My first web project'],
+['title' => 'Project 2', 'description' => 'E-commerce website'],
+['title' => 'Project 3', 'description' => 'Mobile app design'],
+];
+return view('projects', ['projects' => $projects]);
+})->name('projects');
+
+Route::get('/contact', function () {
+$email = 's.pinatacan.johnrio@cmu.edu.ph'; 
+$phone = '+63 935 786 4303'; 
+
+return view('contact', [
+'email' => $email,
+'phone' => $phone
+]);
+})->name('contact');
